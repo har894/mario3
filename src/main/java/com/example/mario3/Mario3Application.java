25f5adc65ff869420ab4497456e099e6c850eb71
HarutSahakyan <hsahakyan94@gmail.com>
2021-08-08 16:55:34 +0400

    Initial commit (Spring boot project)

---
+++ src/main/java/com/example/mario3/Mario3Application.java
@@ -0,0 +1,13 @@
+package com.example.mario3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mario3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mario3Application.class, args);
+    }
+
+}
